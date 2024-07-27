87057a799dc717ebc45374835ae495d7e1039a8c
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2024-07-27 14:11:20 +0200

    copy_file with change path astru

---
+++ bashfiles_palma/insects_bash_astru.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+
+#SBATCH --tasks-per-node=8
+
+#SBATCH --partition=gpua100
+
+#SBATCH --mem=32GB
+
+#SBATCH --gres=gpu:1
+
+#SBATCH --time=7-00:00:00
+
+#SBATCH --error=/scratch/tmp/a_stru03/errors/insects
+
+#SBATCH --job-name=insects_job
+
+#SBATCH --output=/scratch/tmp/a_stru03/output/insects
+
+#SBATCH --mail-type=ALL
+
+#SBATCH --mail-user= a_stru03@uni-muenster.de
+
+#load modules with available GPU support (this is an example, modify to your needs!)
+module purge
+module load palma/2021a Miniconda3/4.9.2
+
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda deactivate
+conda activate /home/a/a_stru03/envs/astru
+
+# place of fork in palma
+yh=/home/a/a_stru03/ml_insects/yolov7_fork_01
+# folder with project folders containing pngs and txts
+tb=/scratch/tmp/a_stru03/insects
+data="$yh"/data/insects_pr.yaml
+hyp="$yh"/data/hyp.scratch.p5_insects.yaml
+#yolov7x weights
+weights=/scratch/tmp/a_stru03/pt_weights/yolov7x.pt
+# yolov7x yaml
+cfg="$yh"/cfg/training/yolov7x.yaml
+
+# weights on github
+#
+
+python "$yh"/train.py --workers 4 --device 0 --batch-size 6 --data "$data" --cfg "$cfg"  --weights "$weights" --hyp "$hyp" --single-cls --epochs 100 --img 1280 1280 --name yolov7_insects1  --project "$tb"/runs
+
+conda deactivate
+module purge
---
+++ bashfiles_palma/template_astru.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+#SBATCH --nodes=1                               # the number of nodes you want to reserve
+#SBATCH --ntasks-per-node=1                     # the number of tasks/processes per node
+#SBATCH --cpus-per-task=8                       # the number cpus per task
+#SBATCH --partition=normal                      # on which partition to submit the job
+#SBATCH --time=24:00:00                         # the max wallclock time (time limit your job will run)
+ 
+#SBATCH --job-name=ml_insects                   # the name of your job
+#SBATCH --mail-type=ALL                         # receive an email when your job starts, finishes normally or is aborted
+#SBATCH --mail-user=a_stru03@uni-muenster.de    # your mail address
+ 
+hyp= 
+# LOAD MODULES HERE IF REQUIRED
+# module load ..
+module purge
+# --img 1280 1280
+# -- workers 8 = cpuspertask
+# device = 0
+
+# START THE APPLICATION
+
+# don't use more than 4 cores per GPU!
+# don't use more than 30 GB per GPU reserved. Adjust the reserved time according to your needs.
