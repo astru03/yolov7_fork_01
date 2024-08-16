43c89a350b28c1742c5a0815507ac7945179f046
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2024-08-16 11:33:27 +0200

    modified add comments

--- merge_annotations.py
+++ merge_annotations.py
@@ -4,25 +4,26 @@ import re
 
 
 # Pfad zum Ordner mit den JSON-Dateien
-#json_folder = r"D:\ML_INSec\data_test\annotations\output_tojson\chunkwise\2023-09-28_12-16-27.479006981"
-json_folder= r"D:\ML_INSec\data_test\annotations\output_tojson\chunkwise\2023-10-01_14-05-35.643162290"
+# json_folder = r"D:\ML_INSec\data_test\annotations\output_tojson\chunkwise\2023-09-28_12-16-27.479006981"
+json_folder= r"C:\Users\Andreas\Desktop\Geoinformatik\SEMESTER_6\01_Studienprojekt\annotations\Export_v2_project_ictrap_beamsplitter_8_12_2024\2023-10-15_13-28-13.077287350"
 
-# list of jsons
+# Liste der JSON-Dateien im Ordner
 json_list = os.listdir(json_folder)
 
-# opens first json 
+# Öffnen der ersten JSON-Datei
 with open(os.path.join(json_folder, json_list[0]), 'r') as file:
             json_data = json.load(file)
-# get external id 
+            
+# Abrufen der 'external_id' aus der ersten JSON-Datei
 external_id = json_data.get("data_row", {}).get("external_id")
 if not external_id:
     print(f" keine 'external_id' in {file} \n.")
     
-# get external id without chunk id
+# Entfernen der Chunk-ID aus der 'external_id'. In diesem fall die Bezeichnung mit "_combined"
 pattern = r"(.*?)_combined"
 external_id_without_chunk=re.search(pattern, external_id).group(1)
 
-# Ordner 'allchunks' erstellen, falls er nicht existiert
+#  Ordner 'allchunks' erstellen, falls dieser nicht existiert. In diesem wird die zusammengesetzte neue JSON-Datei abgespeichert
 try:
     allchunks_folder = os.path.join(json_folder, "allchunks")
     if not os.path.exists(allchunks_folder):
@@ -30,75 +31,54 @@ try:
         print(f"Ordner '{allchunks_folder}' erfolgreich erstellt!")
 except OSError as e:
     print(f"Fehler beim Erstellen des Ordners '{allchunks_folder}': {e}")
-    
-# try:
-#    if not os.path.exists(json_folder+"/allchunks"):
-#        os.mkdir(json_folder+"/allchunks")
-#        print(f"Ordner '{json_folder+"/allchunks"}' erfolgreich erstellt!")
-#except OSError as e:
-#    print(f"Fehler beim Erstellen des Ordners '{json_folder+"/allchunks"}': {e}")
-    
-#write new json file for merged id located in subfolder allchunks
+
+# Neue JSON-Datei für die zusammengeführten Daten erstellen und in den Unterordner 'allchunks' speichern
 with open(json_folder+"/allchunks"+f"/allchunks_{external_id_without_chunk}.json", "w") as outfile:
  json.dump({"frames":[]}, outfile)
 outfile.close()
-  
   
 # Schleife über alle JSON-Dateien im Ordner
 current_frame_count = 0
 for json_file in json_list:
     if json_file.endswith(".json"):
         with open(os.path.join(json_folder, json_file), 'r') as file:
-            # chunk id of current json file
+            # Chunk-ID der aktuellen JSON-Datei ermitteln
             parts = json_file.split("combined_")
             chunk_id = parts[1].split("_")[0]
             chunk_id = int(chunk_id)
-            # cuurent json file
+            # Laden der aktuellen JSON-Datei
             current_file = json.load(file)
-            # projects id 
+            # Projekt-ID ermitteln
             projects_id = list(current_file["projects"])[0]
-            # check if json has labels
+            # Überprüfen, ob Labels in der JSON-Datei vorhanden sind
             if ("labels" in current_file["projects"][projects_id] and len(current_file["projects"][projects_id]["labels"]) > 0 ):
                 frames = current_file["projects"][projects_id]["labels"][0]["annotations"]["frames"]
-               
-                #print(f"frame content: {frames}")
-                # old frame ids (string)
+                # Alte Frame-IDs (als String und als Integer)
                 frame_ids = list(frames)
-                # old frame ids (int)
                 ids_int_list = [int(x) for x in frame_ids]
-                #print(f"Chunk number {chunk_id}: \n ")
-                #print(frame_ids, "\n")
-                #print(ids_int_list)
-                #print (f"Difference is: {current_frame_count} \n")
-                # new frame ids (int)
+ 
+                # Berechnen der neuen Frame-IDs, indem die aktuelle Frame-Anzahl hinzugefügt wird
                 ids_int_list_new = [x + current_frame_count for x in ids_int_list] 
-                # new frame ids (string)
                 frame_ids_new = [str(x) for x in ids_int_list_new]
-                #print (frame_ids_new, "\n")
+                
+                # Ersetzen der alten Frame-IDs durch die neuen IDs
                 for (old,new) in zip(frame_ids,frame_ids_new):
-                    
-                        #new_frame ="{'frame_ids_new':{frames[]}}"
                         old_value = frames[old]
                         frames.pop(old)
                         frames[new] = old_value
                         
-                        #print(frames)
-                        #print((old,new,current_frame_count))
+                # Laden der JSON-Datei, in der alle Chunks zusammengeführt werden sollen
                 with open(json_folder+"/allchunks"+f"/allchunks_{external_id_without_chunk}.json", "r+") as outfile:
                     final_json= json.load(outfile)
-                    
+                     # Anhängen der Frames des aktuellen Chunks an die endgültige JSON-Datei
                     final_json["frames"].append(frames)
-                    
                 outfile.close()
-                
+
+                # Speichern der aktualisierten JSON-Datei
                 with open(json_folder+"/allchunks"+f"/allchunks_{external_id_without_chunk}.json", "w") as outfile:
                     json.dump(final_json,outfile, indent="")
-                    
                 outfile.close()
-                
-                    
+
+            # Aktualisieren der aktuellen Frame-Anzahl um die Anzahl der Frames im aktuellen Chunk   
             current_frame_count = current_frame_count + current_file["media_attributes"]["frame_count"]
-                
-            
-            #print(f"current frame count is : {current_frame_count}")
         
