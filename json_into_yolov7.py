b702747501c62e99ab6b333aac374ce2ee05385c
Andreas Struffert-Froböse <a_stru03@uni-muenster.de>
2024-08-16 11:47:49 +0200

    modified add comments

--- json_into_yolov7.py
+++ json_into_yolov7.py
@@ -3,7 +3,7 @@ import os
 import re
 
 # Pfad zur JSON-Datei
-json_file_path = r"C:\Users\Andreas\Desktop\Geoinformatik\SEMESTER_6\01_Studienprojekt\annotations\Export_v2_project_ictrap_beamsplitter_4_19_2024\2023-09-28_12-16-27.479006981\allchunks\allchunks_2023-09-28_12-16-27.479006981.json"
+#json_file_path = r"C:\Users\Andreas\Desktop\Geoinformatik\SEMESTER_6\01_Studienprojekt\annotations\Export_v2_project_ictrap_beamsplitter_4_19_2024\2023-09-28_12-16-27.479006981\allchunks\allchunks_2023-09-28_12-16-27.479006981.json"
 
 # Funktion zur Konvertierung der Koordinaten in das YOLO-Format
 def convert_coordinates(width, height, bbox):
@@ -20,23 +20,23 @@ def create_yolo_labels(json_file_path):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
 
-    # Extrahiere Metadaten
+    # Extrahierte Metadaten. Format des DVS-Video
     height = float(1080)
     width = float(1920)
 
-    # Extrahiere den Basename der JSON-Datei (ohne Pfad und Erweiterung)
+    # Extrahiert den Base_name der JSON-Datei (ohne Pfad und Erweiterung)
     base_filename = os.path.splitext(os.path.basename(json_file_path))[0]
     base_filename = base_filename.replace("allchunks_", "")
     print("base_filename:\n", base_filename)
 
-    # Erstelle einen Ordner für die Frames dieser JSON-Datei
+    # Erstellt einen Ordner für die Frames dieser JSON-Datei
     output_folder = os.path.join(os.path.dirname(json_file_path), base_filename)
     os.makedirs(output_folder, exist_ok=True)
 
-    # Iteriere über die Frames und erstelle für jeden Frame eine Textdatei
+    # Iteriert über die Frames und erstellt für jeden Frame eine Textdatei
     for frame in data["frames"]:
         for frame_num, frame_data in frame.items():
-            # Bestimme die Präfixlänge basierend auf der Anzahl der Ziffern der Framenummer
+            # Bestimmt die Präfixlänge basierend auf der Anzahl der Ziffern der Framenummer
             frame_num_str = str(frame_num)
             if len(frame_num_str) == 1:
                 prefix = "video_0000"
@@ -49,7 +49,7 @@ def create_yolo_labels(json_file_path):
             else:
                 prefix = "video_"
 
-            # Initialisiere den Objektzähler
+            # Initialisiert den Objektzähler
             obj_counter = 0
             yolo_lines = []
 
@@ -57,18 +57,18 @@ def create_yolo_labels(json_file_path):
                 # Abfrage ob "name":"insect" und "value":"insect"
                 if obj_data.get("name") == "insect" and obj_data.get("value") == "insect":
                     bbox = obj_data['bounding_box']
-                    # Konvertiere die Bounding-Box-Koordinaten ins YOLO-Format
+                    # Konvertiert die Bounding-Box-Koordinaten ins YOLO-Format
                     x_center, y_center, bbox_width, bbox_height = convert_coordinates(width, height, bbox)
-                    # Füge die Zeile zur Liste hinzu
+                    # Fügt die Zeile zur Liste hinzu
                     yolo_lines.append(f"{obj_counter} {x_center} {y_center} {bbox_width} {bbox_height}")
-                    # Inkrementiere den Objektzähler
+                    # Inkrementiert den Objektzähler
                     obj_counter += 1
 
-            # Schreibe nur dann eine Datei, wenn es mindestens eine gültige Objektzeile gibt.
+            # Schreibt nur dann eine Datei, wenn es mindestens eine gültige Objektzeile gibt.
             if yolo_lines:
                 output_file = os.path.join(output_folder, f'{prefix}{frame_num_str}.txt')
                 with open(output_file, 'w') as file:
                     file.write("\n".join(yolo_lines))
 
-# Erstelle YOLO-Labels für die gegebene JSON-Datei
+# Erstellt YOLO-Labels für die gegebene JSON-Datei
 create_yolo_labels(json_file_path)
